98cc7cbbdfd74f2ccf884dfe91c5b3fa6fbd1da7
songareum <songlove0403@gmail.com>
2026-06-19 14:13:52 +0900

    Refactor A : DB연결 방법 변경

--- data/load_from_s3.py
+++ data/load_from_s3.py
@@ -16,11 +16,7 @@ load_dotenv()
 # DB 연결 함수
 def connect_db():
     return psycopg2.connect(
-        host=os.getenv('POSTGRES_HOST'),
-        port=os.getenv('POSTGRES_PORT'),
-        dbname=os.getenv('POSTGRES_DB'),
-        user=os.getenv('POSTGRES_USER'),
-        password=os.getenv('POSTGRES_PASSWORD'),
+        os.getenv('DATABASE_URL'),
         keepalives=1,
         keepalives_idle=60,
         keepalives_interval=10,
